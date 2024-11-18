daf04abd53bb21eca495e31d6f0d263b246ce98d
PaToxCo <rodrigoanayaquispe1206@gmail.com>
2024-11-17 22:19:29 -0500

    Eliminar de Consulta

--- src/app/components/consulta/listarconsulta/listarconsulta.component.ts
+++ src/app/components/consulta/listarconsulta/listarconsulta.component.ts
@@ -6,26 +6,52 @@ import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { ConsultaService } from '../../../services/consulta.service';
 import { Consulta } from '../../../models/consulta';
 import { CommonModule } from '@angular/common';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-listarconsulta',
   standalone: true,
-  imports: [MatTableModule, MatPaginatorModule, MatSortModule, MatInputModule, MatInput, CommonModule],
+  imports: [MatTableModule, MatPaginatorModule, MatSortModule, MatInputModule, MatInput, CommonModule, MatSnackBarModule],
   templateUrl: './listarconsulta.component.html',
   styleUrl: './listarconsulta.component.css'
 })
 export class ListarconsultaComponent {
   displayedColumns: string[] = ['c1', 'c2', 'c3', 'c4'];
+  contador: number = 0;
   dataSource: MatTableDataSource<Consulta> = new MatTableDataSource();
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  constructor(private cS: ConsultaService) { }
+  constructor(private cS: ConsultaService,private snackBar: MatSnackBar) { }
 
+  eliminar(id: number) {
+    this.cS.delete(id).subscribe((data) => {
+      this.cS.list().subscribe((data) => {
+        this.cS.setList(data);
+        this.contador = data.length;
+        this.showDeleteSnackbar();
+        if (this.contador == 0) {
+          this.showNoDietasSnackbar();
+        }
+      });
+    });
+  }
   ngOnInit(): void {
     this.cS.list().subscribe((data) => {
+
       this.dataSource.data = data;
+
+      this.dataSource = new MatTableDataSource(data);
+      this.contador = data.length;
+      if (this.contador == 0) {
+        this.showNoDietasSnackbar();
+      }
+
+    });
+    this.cS.getList().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+      this.contador = data.length;
     });
   }  
 
@@ -37,5 +63,19 @@ export class ListarconsultaComponent {
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+  showNoDietasSnackbar() {
+    this.snackBar.open('No hay dietas registradas', 'Cerrar', {
+      duration: 5000,
+      verticalPosition: 'bottom', // Posición en la pantalla
+      horizontalPosition: 'center',
+    });
+  }
+  showDeleteSnackbar() {
+    this.snackBar.open('Eliminado correctamente', 'Cerrar', {
+      duration: 5000,
+      verticalPosition: 'bottom', // Posición en la pantalla
+      horizontalPosition: 'center',
+    });
   }
 }
--- src/app/services/consulta.service.ts
+++ src/app/services/consulta.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Consulta } from '../models/consulta';
+import { Subject } from 'rxjs';
 
 const base_url = environment.base;
 
@@ -10,6 +11,7 @@ const base_url = environment.base;
 })
 export class ConsultaService {
   private url = `${base_url}/consulta`;
+  private listaCambio = new Subject<Consulta[]>();
   constructor(private http: HttpClient) { }
   list() {
     return this.http.get<Consulta[]>(this.url+"/listar");
@@ -17,8 +19,17 @@ export class ConsultaService {
   post(c:Consulta){
     return this.http.post<Consulta>(this.url+"/agregar",c);
   }
+  getList() {
+    return this.listaCambio.asObservable();
+  }
+  setList(listaNueva:Consulta[]) {
+    this.listaCambio.next(listaNueva);
+  }
   delete(id:number){
     return this.http.delete<Consulta>(this.url+"/delete"+"/"+id.toString());
+  }
+  listId(id: number) {
+    return this.http.get<Consulta>(`${this.url}/${id}`);
   }
   modificar(c:Consulta){
   return this.http.put<Consulta>(this.url+"/actualizar", c);
